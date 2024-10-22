7076574ddbbe3b601f9eaf18d5a9de4d0bdf6f4c
Mr_Metalero <danymena7@gmail.com>
2024-10-22 14:03:30 +0200

    V1.0 appIMC

--- AppSaludo/app/src/main/java/com/apli/appsaludo/MainActivity.kt
+++ AppSaludo/app/src/main/java/com/apli/appsaludo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.apli.appsaludo
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
@@ -11,6 +12,7 @@ import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,13 +29,26 @@ class MainActivity : AppCompatActivity() {
         val btnHello:AppCompatButton = findViewById(R.id.btnHello)
         val etName:AppCompatEditText =
             findViewById(R.id.cajatexto)
-        val nameCajaTexto:String = etName.text.toString()
 
-        //LOGCAT Y TOAST
+
+        //LOGCAT, TOAST, CERRAR ACTIVIDAD O SET NULL AL TEXTVIEW, CAMBIAR ACTIVIDAD
         btnHello.setOnClickListener{
             //Log.i("Test","Boton pulsado")
+            val nameCajaTexto:String = etName.text.toString() //Esto tiene que estar aqui o no pillara el texto
+            //Toast.makeText(this,"Hola $nameCajaTexto Que tal?",Toast.LENGTH_LONG).show()
 
-            Toast.makeText(this,"Hola $nameCajaTexto Que tal?",Toast.LENGTH_LONG).show()
+           /* if (nameCajaTexto.isNotEmpty()) {
+                Snackbar.make(btnHello, "Hola $nameCajaTexto!", Snackbar.LENGTH_SHORT)
+                    .setAnchorView(btnHello)
+                    .setAction("CLOSE") { etName.setText(null) }
+                    .show()
+            }*/
+
+            // INICIA A SEGUNDA ACTIVIDAD
+        val intentGA = Intent(this, SecondActivity::class.java)
+            intentGA.putExtra("EXTRA_NAME", nameCajaTexto)
+            startActivity(intentGA)
+
         }
 
 
---
+++ AppSaludo/app/src/main/java/com/apli/appsaludo/SecondActivity.kt
@@ -0,0 +1,27 @@
+package com.apli.appsaludo
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layoutSecond)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvGreeting: TextView = findViewById(R.id.tvGreeting)
+        val name:String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvGreeting.text = "Hola $name"
+
+
+    }
+}
