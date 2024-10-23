8d427003880d4b42a52d424cd7dfb91839acb2f3
Mr_Metalero <danymena7@gmail.com>
2024-10-23 12:22:52 +0200

    V1.1 appIMC

--- ImcApp/app/src/main/java/com/apli/imcapp/ImcCalculatorActivity.kt
+++ ImcApp/app/src/main/java/com/apli/imcapp/ImcCalculatorActivity.kt
@@ -3,10 +3,15 @@ package com.apli.imcapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcCalculatorActivity : AppCompatActivity() {
+    private lateinit var viewMale:CardView
+    private lateinit var viewFemale:CardView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,49 @@ class ImcCalculatorActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        initComponents()
+        initListeners()
+
     }
+
+
+
+    private fun initComponents(){
+        viewMale = findViewById(R.id.viewMale)
+        viewFemale = findViewById(R.id.viewFemale)
+
+    }
+    val cardViewMale = findViewById<CardView>(R.id.viewMale)
+    val cardViewFemale = findViewById<CardView>(R.id.viewFemale)
+
+
+    private fun initListeners(){
+
+        var isComponentSelectedMale = false
+        var isComponentSelectedFemale = false
+
+        cardViewMale.setOnClickListener {
+
+            cardViewMale.isActivated = true
+
+            setGenderColor()
+        }
+
+        cardViewFemale.setOnClickListener {
+            cardViewFemale.isActivated = true
+            setGenderColor()
+        }
+
+    }
+
+    private fun setGenderColor(){
+
+
+    }
+
+
+
+
+
 }
