4482977a1794c50b98eb33fb8c149214bf105357
Mr_Metalero <danymena7@gmail.com>
2024-11-06 14:02:08 +0100

    APP IMC CASI COMPLETA

--- ImcApp/app/src/main/java/com/apli/imcapp/ImcResultActivity.kt
+++ ImcApp/app/src/main/java/com/apli/imcapp/ImcResultActivity.kt
@@ -7,6 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ImcResultActivity : AppCompatActivity() {
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,37 @@ class ImcResultActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+
+        initComponents()
+        initListeners()
+        initUI()
+
     }
+
+    fun initComponents(){
+
+
+    }
+
+
+    fun initListeners(){
+
+
+    }
+
+    fun initUI(){
+
+
+    }
+
+
+
+
+
+
+
+
+
 }
