97111c8681022a02068c5efca295e6794cd1364d
Mr_Metalero <danymena7@gmail.com>
2025-01-08 14:56:43 +0100

    ScaffoldApp Clase 1

--- ScaffoldApp/app/src/main/java/com/apli/scaffoldapp/MainActivity.kt
+++ ScaffoldApp/app/src/main/java/com/apli/scaffoldapp/MainActivity.kt
@@ -5,12 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -23,27 +23,33 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.apli.scaffoldapp.ui.theme.ScaffoldAppTheme
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -51,16 +57,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ScaffoldAppTheme {
-                Scaffold(
-                    topBar = { MyTopAppBar() },
-                    content = { innerPadding ->
-                        MyContent(innerPadding)
-                    },
-                    bottomBar = { MyBottomNavigation() },
-                    floatingActionButtonPosition = FabPosition.End,
-                    floatingActionButton = { MyFAB() }
-                )
+                val scope = rememberCoroutineScope()
+                var drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                ModalNavigationDrawer(
+                    drawerState = drawerState,
+                    gesturesEnabled = true,
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            MyNavigationDrawer() { scope.launch { drawerState.close() } }
+                        }
+                    }
+                ) {
+                    Scaffold(
+                        topBar = { MyTopAppBar { scope.launch { drawerState.open() } } },
+                        content = { innerPadding ->
+                            MyContent(innerPadding)
+                        },
+                        bottomBar = { MyBottomNavigation() },
+                        floatingActionButtonPosition = FabPosition.End,
+                        floatingActionButton = { MyFAB() }
+                    )
+                }
             }
+        }
+    }
+}
+
+@Composable
+fun MyNavigationDrawer(onCloseDrawer: () -> Unit) {
+    Column(modifier = Modifier.padding(8.dp)) {
+        repeat(5) {
+            Text(
+                text = "Opción ${it + 1}",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp)
+                    .clickable { onCloseDrawer() }
+            )
         }
     }
 }
@@ -121,7 +154,7 @@ fun MyFAB() {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyTopAppBar() {
+fun MyTopAppBar(openDrawer: () -> Job) {
     TopAppBar(
         title = { Text("Top App Bar") },
         colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.LightGray),
@@ -129,7 +162,7 @@ fun MyTopAppBar() {
             IconButton(onClick = { }) { Icon(Icons.Filled.Menu, contentDescription = "Desc") }
         },
         actions = {
-            IconButton(onClick = {}) { Icon(Icons.Filled.Add, contentDescription = "Desc") }
+            IconButton(onClick = {openDrawer()}) { Icon(Icons.Filled.Add, contentDescription = "Desc") }
             IconButton(onClick = {}) { Icon(Icons.Filled.Close, contentDescription = "Desc") }
 
         }
